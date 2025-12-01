9aa61d59a69ae6b9127a2b2ccef42583c4b7f402
monbayashi <monba843@gmail.com>
2025-12-01 15:17:47 +0900

    feature: invitation-service環境変数勝利設定 (#4)

--- apps/invitation-service/src/app.module.ts
+++ apps/invitation-service/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InvitationService } from './invitation/invitation.service';
+import { TypedConfigModule } from './common/config/typed-config.module';
 
 @Module({
-  imports: [],
+  imports: [TypedConfigModule],
   providers: [InvitationService],
 })
 export class AppModule {}
--- apps/invitation-service/src/invitation/invitation.service.spec.ts
+++ apps/invitation-service/src/invitation/invitation.service.spec.ts
@@ -1,13 +1,5 @@
-import { InvitationService } from './invitation.service';
-
 // src/example.spec.ts
 describe('InvitationService', () => {
-  let service: InvitationService;
-  let loggerSpy: jest.SpyInstance;
-
-  beforeEach(() => {
-    service = new InvitationService();
-  });
   it('works', () => {
     expect(true).toBe(true);
   });
