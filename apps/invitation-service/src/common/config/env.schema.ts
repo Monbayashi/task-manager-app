fec2f47c7bd12e99a90bfd4d359e0e03d79daf78
monbayashi <monba843@gmail.com>
2025-12-02 14:35:22 +0900

    feature: invitation-service ロガー設定 (#3)

--- apps/invitation-service/src/common/config/env.schema.ts
+++ apps/invitation-service/src/common/config/env.schema.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 /** 環境変数 Schema */
 export const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']),
+  // INVITATION
+  INVITATION_LOG_LEVEL: z.enum(['error', 'warn', 'info', 'debug', 'silent']),
   // AWS
   AWS_REGION: z.string().min(1),
   AWS_SNS_ENDPOINT: z.string().optional(),
--- apps/invitation-service/src/main.ts
+++ apps/invitation-service/src/main.ts
@@ -3,28 +3,25 @@ import { AppModule } from './app.module';
 import { DynamoDBStreamEvent } from 'aws-lambda';
 import { InvitationService } from './invitation/invitation.service';
 import { INestApplicationContext } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 let appContext: INestApplicationContext | undefined;
 
 export const handler = async (event: DynamoDBStreamEvent) => {
-  try {
-    console.log('☆=== Lambda START 【v25】 ===');
-    console.log('Raw event:', JSON.stringify(event, null, 2));
-    const start = Date.now();
-
-    if (!appContext) {
-      console.log('☆Creating NestJS app context...');
-      appContext = await NestFactory.createApplicationContext(AppModule);
-      console.log('NestJS context created');
-    }
-    const service = appContext.get(InvitationService);
-    const data = service.parse(event);
-    await service.sendEmail(data);
-    console.log('☆sendEmail executed');
-    const end = Date.now();
-    console.log(`☆=== Lambda END 【v25】===: ${end - start}ms`);
-    return { ok: true }; // Lambda が終了したことを明示
-  } catch (err) {
-    console.error('☆Lambda ERROR:', err);
+  if (!appContext) {
+    appContext = await NestFactory.createApplicationContext(AppModule, { bufferLogs: true });
   }
+  // logger
+  const logger = appContext.get(Logger);
+  appContext.useLogger(logger);
+  appContext.flushLogs();
+  // 処理開始
+  logger.log('=== Lambda START 【v25】 ===');
+  logger.log(event);
+  const service = appContext.get(InvitationService);
+  const data = service.parse(event);
+  await service.sendEmail(data);
+  logger.log('sendEmail executed');
+  logger.log(`=== Lambda END 【v25】===`);
+  return { ok: true };
 };
