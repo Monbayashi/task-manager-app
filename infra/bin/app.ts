6dd3cb473a026c2d3970889b934d4a93079751e0
monbayashi <monba843@gmail.com>
2025-12-10 02:20:06 +0900

    fix: di add deploy-stack

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -14,6 +14,7 @@ const CERTIFICATE_ARN =
   "arn:aws:acm:us-east-1:421748860658:certificate/c97cc231-7fee-425e-806c-c928fc3441f8";
 
 // コンテキストから環境を取得
+const deployTarget = app.node.tryGetContext("deploy-stack") || "All";
 const envKey = app.node.tryGetContext("env") || "local";
 const isLocal = envKey === "local";
 
@@ -26,34 +27,40 @@ const env = {
 const dynamoStack = new DynamoDBStack(app, "DynamoDBStack", { env });
 
 // LambdaはinvitationTableのStreamを監視
-new LambdaStack(app, "LambdaStack", {
-  env,
-  table: dynamoStack.invitationTable,
-  domainName: DOMAIN_NAME,
-});
+if (deployTarget === "Lambda" || deployTarget === "All") {
+  new LambdaStack(app, "LambdaStack", {
+    env,
+    table: dynamoStack.invitationTable,
+    domainName: DOMAIN_NAME,
+  });
+}
 
 // 本番専用: localstack不可
 if (!isLocal) {
   new CognitoStack(app, "CognitoStack", { env });
   const networkStack = new NetworkStack(app, "NetworkStack", { env });
   // バックエンドスタックを作成
-  const backendStack = new BackendStack(app, "BackendStack", {
-    env,
-    vpc: networkStack.vpc,
-    cluster: networkStack.cluster,
-    alb: networkStack.alb,
-    invitationTable: dynamoStack.invitationTable,
-    taskTable: dynamoStack.taskTable,
-    domainName: DOMAIN_NAME,
-  });
+  if (deployTarget === "Backend" || deployTarget === "All") {
+    new BackendStack(app, "BackendStack", {
+      env,
+      vpc: networkStack.vpc,
+      cluster: networkStack.cluster,
+      alb: networkStack.alb,
+      invitationTable: dynamoStack.invitationTable,
+      taskTable: dynamoStack.taskTable,
+      domainName: DOMAIN_NAME,
+    });
+  }
 
   // フロントエンド/配信スタックを作成
-  new FrontendStack(app, "FrontendStack", {
-    env,
-    alb: networkStack.alb,
-    domainName: DOMAIN_NAME,
-    certificateArn: CERTIFICATE_ARN,
-  });
+  if (deployTarget === "Frontend" || deployTarget === "All") {
+    new FrontendStack(app, "FrontendStack", {
+      env,
+      alb: networkStack.alb,
+      domainName: DOMAIN_NAME,
+      certificateArn: CERTIFICATE_ARN,
+    });
+  }
 }
 
 app.synth();
